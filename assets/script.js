3d410d14c007f9b353317ed922de57bf4c7e015a
vv-qian <v.v.qian@gmail.com>
2020-09-04 09:46:31 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -7,41 +7,5 @@ projectContainerIDs.map((id) => {
   // On click
   $(`#${id}`).click(() => {
     history.replaceState(null, null, `${document.location.pathname}#${id}`);
-    toggleDetails();
   });
-
-  // On hover
-  $(`#${id}`).hover(
-    function () {
-      if (
-        $(this).find(".details").hasClass("hidden") ||
-        !$(this).find(".details").length
-      ) {
-        $(this).addClass("hover");
-      }
-    },
-    function () {
-      $(this).removeClass("hover");
-    }
-  );
-});
-
-function toggleDetails() {
-  if (location.hash) {
-    $(`${location.hash} .details`).toggleClass("hidden");
-    document
-      .getElementById(`${location.hash.replace("#", "")}`)
-      .scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-        inline: "nearest",
-      });
-  }
-}
-
-window.addEventListener("hashchange", toggleDetails, false);
-
-// On load
-$(document).ready(function () {
-  toggleDetails();
 });
